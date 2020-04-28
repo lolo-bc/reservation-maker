ca1ed32bc179dda10474a51e3df37bcffabfc56b
lolobc <lauren.n.cameron@gmail.com>
2020-04-27 20:24:43 -0400

    push

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var PORT = process.env.PORT || 4000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
+//(DATA)
 // =============================================================
 var reservations = [
   {
@@ -24,6 +24,17 @@ var reservations = [
   },
  
 ];
+
+var waitlist = [
+    {
+      routeName: "lateso",
+      name: "Late person",
+      number: "555-555-5555",
+      email: "email@theinternet.com",
+      uniqueID: "lateso"
+    },
+   
+  ];
 
 // Routes
 // =============================================================
@@ -41,25 +52,14 @@ app.get("/viewres", function(req, res) {
     res.sendFile(path.join(__dirname, "viewres.html"));
   });
 
-// Displays all characters
 app.get("/api/reservations", function(req, res) {
   return res.json(reservations);
 });
 
-// Displays a single character, or returns false
-app.get("/api/reservations/:reservation", function(req, res) {
-  var chosen = req.params.character;
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+  });
 
-  console.log(chosen);
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
-});
 
 app.post("/api/reservations", function(req, res) {
 
@@ -71,6 +71,10 @@ app.post("/api/reservations", function(req, res) {
   reservations.push(newReservation);
 
   res.json(newReservation);
+
+  if (reservations.length >= 5) {
+    waitlist.push(newReservation);
+  }
 });
 
 // Starts the server to begin listening
